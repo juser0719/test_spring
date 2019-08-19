e6d0a864edc690b8f6c52460912e5616dc82f2e2
juser0719 <sinsa0719@naver.com>
2019-08-19 14:50:44 +0900

    jeon

--- src/main/java/com/example/test_spring/controller/openweather.java
+++ src/main/java/com/example/test_spring/controller/openweather.java
@@ -1,18 +1,21 @@
 package com.example.test_spring.controller;
 
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-public class RestOpenWeather {
-    @RequestMapping(value = "/remote/weather", method = RequestMethod.POST)
-    public Object actionMethod(){
+@CrossOrigin
+@RestController
+public class openweather {
+    @RequestMapping(value = "/weather", method = RequestMethod.GET)
+    public Object actionMethod()
+    {
         RestTemplate restTemplate = new RestTemplate();
         Object resultObject = new Object();
         String keyid = "a2efb471990ef1e48ea7ff2b96f9f3c3";
-        String targeturi = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid="+keyid;
+        String targeturi = "https://samples.openweathermap.org/data/2.5/forecast?id=1835848&appid=" + keyid;
         resultObject = restTemplate.getForObject(targeturi, Object.class);
         return resultObject;
     }
